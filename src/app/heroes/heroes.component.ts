1b99db2ee185a896e188bf8f4372e18b3d31c1d4
yamu <ttobiyam@nate.com>
2018-06-30 17:01:23 +0900

    1장 컴포넌트 생성 2장 문법 3장 활용

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,11 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero = 'Winstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Winstorm'
+  };
+
+  isSpecial = true;
+
+  heroes = HEROES;
+
+  selectedHero: Hero; // 디테일 뷰를 렌더링 하기 위한 모델정보
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSave(event: Event) {
+    console.log(event);
   }
 
 }
